1f4c51a73aa68482e25011a96e732c33e810c88c
divyansh <divyanshb22@gmail.com>
2020-10-28 12:25:16 +0530

    add thread for the Rabbit

--- src/main/Race.java
+++ src/main/Race.java
@@ -1,7 +1,10 @@
 package main;
 
+import thread.rabbit.Rabbit;
+
 public class Race {
     public static void main(String[] args) {
-
+        Rabbit rabbit = new Rabbit();
+        rabbit.getRabbit().start();
     }
 }
--- src/thread/rabbit/Rabbit.java
+++ src/thread/rabbit/Rabbit.java
@@ -1,4 +1,38 @@
 package thread.rabbit;
 
-public class Rabbit {
+import javax.swing.*;
+
+public class Rabbit implements Runnable {
+
+    private static final int MILESTONES = 5;
+
+    private final Thread rabbit;
+
+    public Rabbit() {
+        this.rabbit = new Thread(this, "Rabbit");
+    }
+
+    public Thread getRabbit() {
+        return rabbit;
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            System.out.printf("The Rabbit has completed %.2f %% of the race!\n",
+                    index * (100.0f / MILESTONES)
+            );
+            try {
+                Thread.sleep(1500L);
+            } catch (InterruptedException e) {
+                System.err.println("The rabbit has lost the path!");
+            }
+        }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Rabbit has completed the race!",
+                "Rabbit",
+                JOptionPane.OK_CANCEL_OPTION
+        );
+    }
 }
